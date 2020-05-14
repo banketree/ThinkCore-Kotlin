458b96c4bdfdbbd7be5920b4c40fdf8ea223b390
半棵树 <banketree@gmail.com>
2020-05-14 11:04:02 +0800

    Update IKeyDownListener.kt

--- thinkActivity/src/main/java/com/thinkcore/activity/IKeyDownListener.kt
+++ thinkActivity/src/main/java/com/thinkcore/activity/IKeyDownListener.kt
@@ -5,3 +5,10 @@ import android.view.KeyEvent
 interface IKeyDownListener {
     fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean
 }
+
+inline fun onKeyDownListener(crossinline iActivityResult: (keyCode: Int, event: KeyEvent) -> Boolean): IKeyDownListener {
+    return object : IKeyDownListener {
+        override fun onKeyDown(keyCode: Int, event: KeyEvent): Boolean =
+            iActivityResult.invoke(keyCode, event)
+    }
+}
